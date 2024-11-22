c6e52674725674eb024cf6abef30787a7260363f
Anastasia Shapenkova <wondernasty@yandex.ru>
2024-11-22 15:24:47 +0500

    добавила блок в корзину и функционал

--- src/components/DrawerBlock.vue
+++ src/components/DrawerBlock.vue
@@ -2,28 +2,39 @@
   <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
     <DrawerHead />
-    <CartListItem />
 
-    <div class="flex flex-col gap-4 my-7">
-      <div class="flex gap-2">
-        <span>Итого:</span>
-        <div class="flex-1 border-b border-dashed"></div>
-        <b>{{ totalPrice }} &#8381;</b>
+    <div v-if="!totalPrice" class="flex h-full items-center">
+      <InfoBlock
+        title="Корзина пустая"
+        description="Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ."
+        image-url="/package-icon.png"
+      />
+    </div>
+
+    <div v-else>
+      <CartListItem />
+
+      <div class="flex flex-col gap-4 my-7">
+        <div class="flex gap-2">
+          <span>Итого:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>{{ totalPrice }} &#8381;</b>
+        </div>
+
+        <div class="flex gap-2">
+          <span>Налог 5%:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>{{ vatPrice }} &#8381;</b>
+        </div>
+
+        <button
+          :disabled="buttonDisabled"
+          @click="() => emit('createOrder')"
+          class="bg-lime-500 w-full rounded-xl py-3 text-white hover:bg-lime-600 transition active:bg-lime-700 disabled:bg-slate-300 cursor-pointer mt-4"
+        >
+          Оформить заказ
+        </button>
       </div>
-
-      <div class="flex gap-2">
-        <span>Налог 5%:</span>
-        <div class="flex-1 border-b border-dashed"></div>
-        <b>{{ vatPrice }} &#8381;</b>
-      </div>
-
-      <button
-        :disabled="buttonDisabled"
-        @click="() => emit('createOrder')"
-        class="bg-lime-500 w-full rounded-xl py-3 text-white hover:bg-lime-600 transition active:bg-lime-700 disabled:bg-slate-300 cursor-pointer mt-4"
-      >
-        Оформить заказ
-      </button>
     </div>
   </div>
 </template>
@@ -31,6 +42,7 @@
 <script setup>
 import DrawerHead from './DrawerHead.vue'
 import CartListItem from './CartListItem.vue'
+import InfoBlock from './InfoBlock.vue'
 
 const emit = defineEmits(['createOrder'])
 
